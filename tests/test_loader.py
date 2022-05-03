118e8ead20b1579fa3ccfcac40f8934506398828
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-03 15:40:07 +0300

    Add a test of downloading local images

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -3,25 +3,58 @@ import tempfile
 
 import requests_mock
 from page_loader.loader import download
+from tests.utils import load_fixture
 
 
 def test_download():
-    path = './tests/fixtures/page.html'
-    page_url = 'https://ethereum.org/en'
+    path_to_orig_page = './tests/fixtures/page.html'
+    page_url = 'https://ru.hexlet.io/courses'
 
-    with open(path, 'r') as file:
-        content = file.read()
+    src_png = load_fixture('./tests/fixtures/page_sources/python.png', True)
 
     tmpdir = tempfile.TemporaryDirectory()
     tmpdirname = tmpdir.name
 
-    page_file_path = os.path.join(tmpdir.name, 'ethereum-org-en.html')
+    page_file_path = os.path.join(tmpdirname, 'ru-hexlet-io-courses.html')
 
-    with requests_mock.Mocker() as mock:
-        mock.get(page_url, text=content)
+    mocks = [
+        (
+            page_url,
+            load_fixture(path_to_orig_page)
+        ),
+        (
+            'https://ru.hexlet.io/assets/professions/python.png',
+            src_png
+        )
+    ]
+
+    with requests_mock.Mocker() as mocker:
+        for url, content in mocks:
+            if isinstance(content, bytes):
+                mocker.get(url, content=content)
+            else:
+                mocker.get(url, text=content)
+
         assert download(page_url, tmpdirname) == page_file_path
 
-    with open(page_file_path) as file:
-        assert file.read() == content
+    resources_dir = 'ru-hexlet-io_files/'
+    resources = [
+        (
+            'ru-hexlet-io-courses.html',
+            load_fixture('./tests/fixtures/page_after.html')
+        ),
+        (
+            resources_dir + 'ru-hexlet-io-assets-professions-python.png',
+            src_png
+        )
+    ]
+
+    for file_name, content in resources:
+        path_to_file_name = os.path.join(tmpdirname, file_name)
+
+        read_mode = 'rb' if isinstance(content, bytes) else 'r'
+
+        with open(path_to_file_name, read_mode) as file:
+            assert file.read() == content
 
     tmpdir.cleanup()
