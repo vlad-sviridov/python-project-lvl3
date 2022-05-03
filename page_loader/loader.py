8bc6e7b6a730062f161849b6d1ce4304df710015
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-03 15:29:18 +0300

    Add functionality that downloads images

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,5 +1,16 @@
 import os
-from page_loader.creating import create_file_name
+
+from page_loader.parsing import parse_and_modify_html
+from page_loader.storage import create_dir, create_file
+from page_loader.urls import get_response, url_to_filename
+
+
+def download_resoures(sources: dict, parent_node: str = ''):
+    for info in sources:
+        content = get_response(info['url']).content
+        src_dir, _ = os.path.split(info['download_path'])
+        create_dir(parent_node + '/' + src_dir)
+        create_file(content, parent_node + '/' + info['download_path'])
 
 
 def download(url: str, output_dir: str) -> str:
@@ -13,9 +24,12 @@ def download(url: str, output_dir: str) -> str:
         str: Path to saved the web page.
     """
 
-    page_content = get_response(url).text
-    file_name = create_file_name(url)
-    path_to_file = os.path.join(output_dir, file_name)
-    save_web_page(page_content, path_to_file)
+    page_orig_content = get_response(url).text
+    page_file_name = url_to_filename(url)
+    path_to_page_file = os.path.join(output_dir, page_file_name)
 
-    return path_to_file
+    page_mod_cnt, res_of_page = parse_and_modify_html(page_orig_content, url)
+    download_resoures(res_of_page, output_dir)
+    create_file(page_mod_cnt, path_to_page_file)
+
+    return path_to_page_file
