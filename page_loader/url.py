314f2067fd69d696adbcc734acd62dcee9782d1e
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-09 16:00:52 +0300

    Refactoring

--- page_loader/url.py
+++ page_loader/url.py
@@ -1,31 +1,29 @@
 import os
 import re
-from urllib import parse
+from urllib.parse import urlparse
 
 
 def url_to_name(url: str) -> str:
-    parsed_url = parse.urlparse(url)
+    parsed_url = urlparse(url)
     raw = parsed_url.netloc + parsed_url.path
-    return re.sub(r'[^0-9a-zA-Z]', '-', raw)
+    return re.sub(r'[^0-9a-zA-Z_]', '-', raw)
 
 
 def url_to_filename(url: str, default_ext: str = '.html') -> str:
-    url = url.rstrip('/')
-    _, ext = os.path.splitext(parse.urlparse(url).path)
+    _, ext = os.path.splitext(url)
     if not ext:
         url_without_ext = url
         ext = default_ext
     else:
-        url_without_ext = url[0:-len(ext)]
+        url_without_ext = url[:-len(ext)]
     return url_to_name(url_without_ext) + ext
 
 
+def url_to_dirname(url: str, postfix: str = '_files') -> str:
+    url = url.rstrip('/')
+    return url_to_name(url) + postfix
+
+
 def get_root_url(url: str) -> str:
-    parsed_url = parse.urlparse(url)
+    parsed_url = urlparse(url)
     return '://'.join((parsed_url.scheme, parsed_url.hostname))
-
-
-def is_local_url(url: str, root_url: str):
-    url_hostname = parse.urlparse(url).hostname
-    root_url_hostname = parse.urlparse(root_url).hostname
-    return url_hostname is None or url_hostname == root_url_hostname
