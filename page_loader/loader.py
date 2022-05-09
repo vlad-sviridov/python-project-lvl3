10be22c540dd2ed9aebd82126ffa154a2bb376c3
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-09 15:54:16 +0300

    Refactoring

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -5,12 +5,12 @@ from page_loader.storage import create_dir, save_file
 from page_loader.url import url_to_filename
 
 
-def download_resoures(sources: dict, parent_node: str = ''):
+def download_resoures(sources: dict, directory: str = ''):
     for info in sources:
         content = requests.get(info['url']).content
         src_dir, _ = os.path.split(info['download_path'])
-        create_dir(parent_node + '/' + src_dir)
-        save_file(content, parent_node + '/' + info['download_path'])
+        create_dir(directory + '/' + src_dir)
+        save_file(content, directory + '/' + info['download_path'])
 
 
 def download(url: str, output_dir: str) -> str:
@@ -24,12 +24,12 @@ def download(url: str, output_dir: str) -> str:
         str: Path to saved the web page.
     """
 
-    page_orig_content = requests.get(url).text
-    page_file_name = url_to_filename(url)
-    path_to_page_file = os.path.join(output_dir, page_file_name)
+    html = requests.get(url).text
+    file_name = url_to_filename(url)
+    path_to_file = os.path.join(output_dir, file_name)
 
-    page_mod_cnt, res_of_page = get_and_replace(page_orig_content, url)
+    modified_html, res_of_page = get_and_replace(html, url)
     download_resoures(res_of_page, output_dir)
-    save_file(page_mod_cnt, path_to_page_file)
+    save_file(modified_html, path_to_file)
 
-    return path_to_page_file
+    return path_to_file
