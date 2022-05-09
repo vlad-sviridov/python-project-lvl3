4ee9bd855bf00bdf7bfdabe4c4e6dd8c5d712f75
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-09 15:58:46 +0300

    Refactoring

--- page_loader/storage.py
+++ page_loader/storage.py
@@ -2,6 +2,9 @@ import os
 from typing import Union
 
 
+DIR_MODE = 0o744
+
+
 def save_file(content: Union[str, bytes], path: str) -> None:
     write_mode = 'wb' if isinstance(content, bytes) else 'w'
     with open(path, write_mode) as file:
@@ -10,4 +13,4 @@ def save_file(content: Union[str, bytes], path: str) -> None:
 
 def create_dir(path: str) -> None:
     if not os.path.exists(path):
-        os.mkdir(path, mode=744)
+        os.mkdir(path, mode=DIR_MODE)
