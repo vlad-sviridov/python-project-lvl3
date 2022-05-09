5776d8827bdfe21cca16b88bcaa115b9bc9b7bc8
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-09 16:09:01 +0300

    Add check of dowload  all resources

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -10,7 +10,10 @@ def test_download():
     path_to_orig_page = './tests/fixtures/page.html'
     page_url = 'https://ru.hexlet.io/courses'
 
-    src_png = load_fixture('./tests/fixtures/page_sources/python.png', True)
+    python_png = load_fixture('./tests/fixtures/page_sources/python.png', True)
+    application_css = load_fixture(
+        './tests/fixtures/page_sources/application.css')
+    runtime_js = load_fixture('./tests/fixtures/page_sources/runtime.js')
 
     tmpdir = tempfile.TemporaryDirectory()
     tmpdirname = tmpdir.name
@@ -24,10 +27,19 @@ def test_download():
         ),
         (
             'https://ru.hexlet.io/assets/professions/python.png',
-            src_png
+            python_png
+        ),
+        (
+            'https://ru.hexlet.io/assets/application.css',
+            application_css
+        ),
+        (
+            'https://ru.hexlet.io/packs/js/runtime.js',
+            runtime_js
         )
     ]
 
+# Check a path of web page
     with requests_mock.Mocker() as mocker:
         for url, content in mocks:
             if isinstance(content, bytes):
@@ -37,15 +49,30 @@ def test_download():
 
         assert download(page_url, tmpdirname) == page_file_path
 
-    resources_dir = 'ru-hexlet-io_files/'
+# Compare html of web page
+    path_to_page = os.path.join(tmpdirname, 'ru-hexlet-io-courses.html')
+    page_html = load_fixture('./tests/fixtures/page_after.html')
+    with open(path_to_page, 'r') as file:
+        assert file.read() == page_html
+
+    # Check a path of web page resources
+    resources_dir = 'ru-hexlet-io-courses_files/'
     resources = [
         (
-            'ru-hexlet-io-courses.html',
-            load_fixture('./tests/fixtures/page_after.html')
+            resources_dir + 'ru-hexlet-io-courses.html',
+            load_fixture('./tests/fixtures/page.html')
         ),
         (
             resources_dir + 'ru-hexlet-io-assets-professions-python.png',
-            src_png
+            python_png
+        ),
+        (
+            resources_dir + 'ru-hexlet-io-assets-application.css',
+            application_css
+        ),
+        (
+            resources_dir + 'ru-hexlet-io-packs-js-runtime.js',
+            runtime_js
         )
     ]
 
