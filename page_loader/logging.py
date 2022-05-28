08ff73f7cd852cbcd67c4a8011c69a5d9c4d70a5
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-28 20:52:08 +0300

    Update logging congfig

--- page_loader/logging.py
+++ page_loader/logging.py
@@ -1,8 +1,10 @@
 import logging
+import sys
 
 
 def setup() -> None:
     logging.basicConfig(
         level=logging.WARNING,
+        stream=sys.stderr,
         format='%(asctime)s - %(levelname)s - %(levelno)s - %(message)s'
     )
