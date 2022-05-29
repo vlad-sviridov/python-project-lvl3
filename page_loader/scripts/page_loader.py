c17c64a976e8200387065f4a6e23de3c7f4af105
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-29 17:42:20 +0300

    Add sys.exit(1)

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import logging
 import logging.config
+import sys
 
 from page_loader.cli import get_parser
 from page_loader.loader import download
@@ -15,6 +16,7 @@ def main():
         output_dir = download(args.url, args.output)
     except Exception as e:
         logging.error('Download failed %s', e)
+        sys.exit(1)
     else:
         print('Page was successfully downloaded into %s', output_dir)
 
