74c871ff857d96f9665d4c25f9d9b2cfbf313868
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2022-05-30 17:41:53 +0300

    Add progress bar

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -5,10 +5,17 @@ import requests
 from page_loader.resources import get_and_replace
 from page_loader.storage import create_dir, save_file
 from page_loader.url import url_to_filename
+from progress.bar import IncrementalBar
 from requests.exceptions import RequestException
 
 
-def download_resoures(sources: dict, directory: str = '') -> None:
+def download_resources(sources: dict, directory: str = '') -> None:
+    bar = IncrementalBar(
+        'Downloading the page',
+        max=len(sources),
+        suffix='%(percent)d%%'
+    )
+
     for info in sources:
         try:
             resp = requests.get(info['url'])
@@ -17,8 +24,11 @@ def download_resoures(sources: dict, directory: str = '') -> None:
             src_dir, _ = os.path.split(info['download_path'])
             create_dir(directory + '/' + src_dir)
             save_file(content, directory + '/' + info['download_path'])
+            logging.info('Downloaded resource %s', info['url'])
+            bar.next()
         except (RequestException, OSError):
             logging.warning('Failed downloaded resource %s', info['url'])
+    bar.finish()
 
 
 def download(url: str, output_dir: str) -> str:
@@ -33,7 +43,7 @@ def download(url: str, output_dir: str) -> str:
     """
 
     try:
-        resp = requests.get(url)
+        resp = requests.get(url, stream=True)
         resp.raise_for_status()
     except RequestException as e:
         logging.error('Error request, Error Message: %s', str(e))
@@ -42,8 +52,9 @@ def download(url: str, output_dir: str) -> str:
     file_name = url_to_filename(url)
     path_to_file = os.path.join(output_dir, file_name)
 
+    logging.info('Started to downloading web page %s', url)
     modified_html, res_of_page = get_and_replace(resp.text, url)
-    download_resoures(res_of_page, output_dir)
+    download_resources(res_of_page, output_dir)
     save_file(modified_html, path_to_file)
 
     return path_to_file
